69f4ae076aa86fc8a945661a64201e1e749c6ae4
Christian <c.sangwa@yahoo.fr>
2022-11-09 03:14:57 -0800

    adjusted2

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,32 +1,72 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 /**
-*main - adds positive numbers
-*@argc: number of arguments
-*@argv: array of arguments
-*Return: 0 on success, 1 on failure
-*/
-
-int main(int argc, char *argv[])
+ * coinConverter - Helper function that does all the mathematics
+ * @i: Passed in variable from main for calculations
+ * Return: The number of coins needed minimum for the passed in variable
+ */
+int coinConverter(int i)
 {
-	int i, j, sum = 0;
+	int count = 0;
 
-	for (i = 1; i < argc; i++)
+	while (i != 0)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (i % 10 == 9 || i % 10 == 7)
+			i -= 2;
+		else if (i % 25 == 0)
+			i -= 25;
+		else if (i % 10 == 0)
+			i -= 10;
+		else if (i % 5 == 0)
+			i -= 5;
+		else if (i % 2 == 0)
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
+			if (i % 10 == 6)
+				i -= 1;
+			else
+				i -= 2;
 		}
+		else
+			i -= 1;
 
-		sum += atoi(argv[i]);
+		count++;
 	}
 
-	printf("%d\n", sum);
+	return (count);
+}
+
+/**
+ * main - Takes in exactly one argument for minimum coin count
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int i, coin;
+
+	coin = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+
+	if (i < 0)
+		printf("0\n");
+	else
+	{
+		coin = coinConverter(i);
+
+		printf("%d\n", coin);
+	}
 
 	return (0);
 }
