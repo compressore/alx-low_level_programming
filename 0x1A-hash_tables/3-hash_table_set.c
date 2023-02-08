be0ef013f01a53ff96e0d094686d2ff52cf45c5d
Christian <c.sangwa@yahoo.fr>
2023-02-07 23:29:12 -0800

    twik

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,45 +1,78 @@
-#include <stdlib.h>
-#include <string.h>
 #include "hash_tables.h"
+
 /**
- * hash_table_set - Creates a new node and assigns key and value to it.
- * @ht: hash table you want to add or update the value/value to
- * @key: the key; cannot be empty
- * @value: value associated with the key.
- * Return: 1 on success, 0 on failure.
- */
+ * hash_table_set - Put a value into the hash table
+ * @ht: The hash table struct
+ * @key: The key used to find the value
+ * @value: The value to add
+ * Return: 1 on success, 0 on fail
+ **/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node, *temp;
 	unsigned long int index;
+	hash_node_t *node;
+	int pass_fail;
 
-	if (key == 0)
-		return (0);
 	if (ht == NULL)
 		return (0);
+	pass_fail = 1;
+	index = key_index((unsigned char *) key, ht->size);
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
-	node->key = strdup(key);
-	node->value = strdup(value);
-	index = key_index((void *)key, ht->size);
-	temp = ht->array[index];
-	if (ht->array[index] != NULL)
+	if (ht->array[index] == NULL)
 	{
-		temp = ht->array[index];
-		while (temp != NULL)
-		{
-			if (strcmp(temp->key, node->key) == 0)
-				break;
-			temp = temp->next;
-		}
-		if (temp == NULL)
-		{
-			node->next = ht->array[index];
-			ht->array[index] = node;
-		}
+		ht->array[index] = node;
+		node->key = strdup(key);
+		if (node->key == NULL)
+			return (0);
+		node->value = strdup(value);
+		if (node->value == NULL)
+			return (0);
+		node->next = NULL;
 	}
 	else
-		node->next = NULL, ht->array[index] = node;
+		pass_fail = check_list((char *) key, ht, index, (char *) value);
+	return (pass_fail);
+}
+
+/**
+ * check_list - check a list at the key's index for the key
+ * @key: Key to look for
+ * @ht: Hash table to look through
+ * @index: Index of the hashed key value
+ * @value: The value associated with the key to add
+ * Return: 1 on success, 0 for failure
+ **/
+int check_list(char *key, hash_table_t *ht,
+	       unsigned long int index, char *value)
+{
+	hash_node_t *current_node;
+	hash_node_t *new_node;
+
+	current_node = ht->array[index];
+	while (current_node != NULL)
+	{
+		if (strcmp(key, current_node->key) == 0)
+		{
+			current_node->value = strdup(value);
+			if (current_node->value == NULL)
+				return (0);
+			return (1);
+		}
+		current_node = current_node->next;
+	}
+	current_node = ht->array[index];
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->next = current_node;
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+		return (0);
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+		return (0);
+	ht->array[index] = new_node;
 	return (1);
 }
