d0e179802be639af7970c65715c26d1d26a17de7
Christian <c.sangwa@yahoo.fr>
2023-12-05 10:28:17 +0200

    fixed header

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,43 @@
-#include "holberton.h"
 
+#include "main.h"
 /**
- * read_textfile - read a text file and print it to stdout
- * @filename: the name of the file to read
- * @letters: the number of letters to be read and printed
- *
- * Return: If filename is NULL, the file cannot be opened or read, or
- * write fails or returns an unexpected number of bytes, return 0.
- * Otherwise, return the actual number of letters read and printed.
- */
+* read_textfile
+* @filename: file to read and write
+* @letters: number of letters to read and write.
+* Return: letters printed
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer = NULL;
-	ssize_t b_read;
-	ssize_t b_written;
-	int fd;
+  ssize_t nletters;
+  int file;
+  char *text;
 
-	if (!(filename && letters))
-		return (0);
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
-		return (0);
-
-	b_read = read(fd, buffer, letters);
-	close(fd);
-
-	if (b_read < 0)
-	{
-		free(buffer);
-		return (0);
-	}
-	if (!b_read)
-		b_read = letters;
-
-	b_written = write(STDOUT_FILENO, buffer, b_read);
-	free(buffer);
-
-	if (b_written < 0)
-		return (0);
-
-	return (b_written);
+  if (!filename)
+    return (0);
+  text = malloc(sizeof(char) * letters + 1);
+  if (text == NULL)
+    return (0);
+  file = open(filename, O_RDONLY);
+  if (file == -1)
+  {
+    free(text);
+    return (0);
+  }
+  nletters = read(file, text, sizeof(char) * letters);
+  if (nletters == -1)
+  {
+    free(text);
+    close(file);
+    return (0);
+  }
+  nletters = write(STDOUT_FILENO, text, nletters);
+  if (nletters == -1)
+  {
+    free(text);
+    close(file);
+    return (0);
+  }
+  free(text);
+  close(file);
+  return (nletters);
 }
