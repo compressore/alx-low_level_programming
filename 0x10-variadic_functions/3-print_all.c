9d04b80551441148f04e0e8be24e115a8befd2f1
Christian <c.sangwa@yahoo.fr>
2023-12-05 10:59:27 +0200

    Write a function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,92 +1,51 @@
 #include "variadic_functions.h"
-
-
+#include <stdio.h> 
 /**
- * print_char - print a character
- * @args: the va_list with the character to print as it's next element
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
  *
- * Return: the number of bytes printed
- */
-int print_char(va_list args)
-{
-	return (printf("%c", va_arg(args, int)));
-}
-
-
-/**
- * print_float - print a float
- * @args: the va_list with the float to print as it's next element
- *
- * Return: the number of bytes printed
- */
-int print_float(va_list args)
-{
-	return (printf("%f", va_arg(args, double)));
-}
-
-
-/**
- * print_int - print an integer
- * @args: the va_list with the integer to print as it's next element
- *
- * Return: the number of bytes printed
- */
-int print_int(va_list args)
-{
-	return (printf("%i", va_arg(args, int)));
-}
-
-
-/**
- * print_str - print a string
- * @args: the va_list with the string to print as it's next element
- *
- * Return: the number of bytes printed
- */
-int print_str(va_list args)
-{
-	const char *str = va_arg(args, const char *);
-
-	if (!str)
-		str = "(nil)";
-	return (printf("%s", str));
-}
-
-
-/**
- * print_all - print anything
- * @format: a format string listing the types of the proceeding arguments
- * @...: the values to print
+ * Return: no return.
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	print_fn_t fn_list[] = {
-		{'c', print_char},
-		{'f', print_float},
-		{'i', print_int},
-		{'s', print_str},
-		{ 0,  NULL}
-	};
-	char *sep[] = {"", ", "};
-	unsigned int bytes = 0, fn_index = 0, format_index = 0;
+  va_list valist;
+  unsigned int i = 0, j, c = 0;
+  char *str;
+  const char t_arg[] = "cifs";
 
-	va_start(args, format);
-	while (format && format[format_index])
-	{
-		fn_index = 0;
-		while (fn_list[fn_index].format)
-		{
-			if (format[format_index] == fn_list[fn_index].format)
-			{
-				printf("%s", sep[bytes != 0]);
-				bytes += fn_list[fn_index].fn(args);
-				break;
-			}
-			++fn_index;
-		}
-		++format_index;
-	}
-	printf("\n");
-	va_end(args);
+  va_start(valist, format);
+  while (format && format[i])
+  {
+    j = 0;
+    while (t_arg[j])
+    {
+      if (format[i] == t_arg[j] && c)
+      {
+        printf(", ");
+        break;
+      } j++;
+    }
+    switch (format[i])
+    {
+    case 'c':
+      printf("%c", va_arg(valist, int)), c = 1;
+      break;
+    case 'i':
+      printf("%d", va_arg(valist, int)), c = 1;
+      break;
+    case 'f':
+      printf("%f", va_arg(valist, double)), c = 1;
+      break;
+    case 's':
+      str = va_arg(valist, char *), c = 1;
+      if (!str)
+      {
+        printf("(nil)");
+        break;
+      }
+      printf("%s", str);
+      break;
+    } i++;
+  }
+  printf("\n"), va_end(valist);
 }
